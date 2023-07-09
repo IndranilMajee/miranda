d86b38b024cbcf023f90c35c33801ba031f8fcc4
IndranilMajee <indranilpro@gmail.com>
2023-07-09 16:01:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+var tl = gsap.timeline();
+
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+tl.to("#page1",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to("#page1",{
+    y:"0vh",
+    rotate:-360,
+    scale:1,
+    duration:0.7
+    
+
+
+})
+// tl.to("#page1",{
+//     y:"-100vh",
+//     duration:0.5,
+//     delay:1
+// })
+
+
+
+
+const container = document.querySelector(".container");
+const cards = document.querySelector(".cards");
+
+/* keep track of user's mouse down and up */
+let isPressedDown = false;
+/* x horizontal space of cursor from inner container */
+let cursorXSpace;
+
+container.addEventListener("mousedown", (e) => {
+  isPressedDown = true;
+  cursorXSpace = e.offsetX - cards.offsetLeft;
+  container.style.cursor = "grabbing";
+});
+
+container.addEventListener("mouseup", () => {
+  container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isPressedDown) return;
+  e.preventDefault();
+  cards.style.left = `${e.offsetX - cursorXSpace}px`;
+  boundCards();
+});
+
+function boundCards() {
+  const container_rect = container.getBoundingClientRect();
+  const cards_rect = cards.getBoundingClientRect();
+
+  if (parseInt(cards.style.left) > 0) {
+    cards.style.left = 0;
+  } else if (cards_rect.right < container_rect.right) {
+    cards.style.left = `-${cards_rect.width - container_rect.width}px`;
+  }
+}
+
